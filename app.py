6d387b12856579ac8dd66c4a97bde8d2c29dec3e
AlbertCalmus <avequin.da@gmail.com>
2022-01-31 10:53:23 +0100

    Full healthcheck

--- app.py
+++ app.py
@@ -1,10 +1,37 @@
+import json
+import subprocess
 from flask import Flask
 
 app = Flask(__name__)
 
+def get_process_output(cmd):
+  res = subprocess.check_output(cmd)
+  return res.decode("utf-8").replace("\n", "").split("=")[-1]
+
 @app.route("/healthcheck")
 def healthcheck():
-  return "Hello world!"
+  VC = "vcgencmd"
+  health = {
+    "clock": {},
+    "mem": {},
+    "volts": {}
+  }
+
+  for src in "arm core h264 isp v3d uart pwm emmc pixel vec hdmi dpi".split():
+    cmd = [VC, "measure_clock", src]
+    health['clock'][src] = get_process_output(cmd)
+  
+  for a in "core sdram_c sdram_i sdram_p".split():
+    cmd = [VC, "measure_volts", a]
+    health['volts'][a] = get_process_output(cmd)
+
+  for a in "arm gpu".split():
+    cmd = [VC, 'get_mem', a]
+    health['mem'][a] = get_process_output(cmd)
+
+  health['temp'] = get_process_output([VC, "measure_temp"])
+  
+  return json.dumps(health)
 
 if __name__ == "__main__":
   app.run(host="0.0.0.0", port=777)
